e1695f825724c4ddd41db5530ca11d792e293bf1
Nekrofage <lesanglierdesardennes@gmail.com>
2024-02-22 23:02:53 +0100

    [Version] 0.0.1

---
+++ Scripts/Entity/player.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+@onready var anim_tree = $AnimationTree
+@onready var anim_state = anim_tree.get("parameters/playback")
+
+var speed = 70
+var input_movement = Vector2.ZERO
+
+func _physics_process(delta):
+	input_move()
+
+
+func input_move():
+	input_movement = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	if input_movement != Vector2.ZERO:
+		anim_tree.set("parameters/Idle/blend_position", input_movement)
+		anim_tree.set("parameters/Move/blend_position", input_movement)
+		anim_state.travel("Move")
+		velocity = input_movement * speed
+
+	if input_movement == Vector2.ZERO:
+		anim_state.travel("Idle")
+		velocity = Vector2.ZERO
+
+	move_and_slide()
