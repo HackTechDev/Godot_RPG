bbdffab556f39d0f925e064b84d6d4cbf53027ce
Nekrofage <lesanglierdesardennes@gmail.com>
2024-02-29 20:04:15 +0100

    wip

--- Scripts/Interactables/entry.gd
+++ Scripts/Interactables/entry.gd
@@ -5,3 +5,9 @@ extends Area2D
 func _on_body_entered(body):
 	if body.name == "Player":
 		get_tree().change_scene_to_file("res://Scenes/Levels/level_02.tscn")
+
+
+func _on_checkpoint_body_entered(body):
+	if body.name == "Player":
+		Player_data.player_position = body.global_position
+		print(Player_data.player_position)
--- Scripts/Levels/level_01.gd
+++ Scripts/Levels/level_01.gd
@@ -1,11 +1,5 @@
 extends Node2D
 
-@onready var player_scene = preload("res://Scenes/Player/player.tscn")
-@onready var spawn_point = $entry/spawn_point
 func _ready():
-	on_player_spawn()
+	pass
 
-func on_player_spawn():
-	var player = player_scene.instantiate()
-	player.position = spawn_point.global_position
-	add_child(player)
