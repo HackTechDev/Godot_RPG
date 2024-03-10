2bf9ebb4f6064a791ca6f53e5e28e1bedeedd397
Nekrofage <lesanglierdesardennes@gmail.com>
2024-03-10 20:56:36 +0100

    [Add] Main menu

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -2,10 +2,13 @@ extends CharacterBody2D
 
 @onready var anim_tree = $AnimationTree
 @onready var anim_state = anim_tree.get("parameters/playback")
+@onready var main_menu = $MainMenu
 
 var speed = 70
 var input_movement = Vector2.ZERO
 var health = Player_data.player_health
+
+var paused
 
 func _ready():
 	pass
@@ -13,7 +16,15 @@ func _ready():
 func _physics_process(_delta):
 	input_move()
 
-
+func _input(event):
+	if !main_menu.visible:
+		if event.is_action_pressed("ui_pause"):
+			get_tree().paused = true
+			main_menu.visible = true
+			set_physics_process(false)
+			paused = true
+			
+				
 func input_move():
 	input_movement = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	
@@ -28,3 +39,41 @@ func input_move():
 		velocity = Vector2.ZERO
 
 	move_and_slide()
+
+
+func _on_resume_pressed():
+	print("resume")
+	#hide pause menu
+	main_menu.visible = false
+	#set pauses state to be false
+	get_tree().paused = false
+	paused = false
+	#accept movement and input
+	set_process_input(true)
+	set_physics_process(true)
+
+
+
+func _on_information_pressed():
+	print("Information")
+	pass # Replace with function body.
+
+
+func _on_configuration_pressed():
+	print("Configuration")
+	pass # Replace with function body.
+
+
+func _on_quit_pressed():
+	print("Quit")
+	pass # Replace with function body.
+
+
+func _on_help_pressed():
+	print("Help")
+	pass # Replace with function body.
+
+
+func _on_inventory_pressed():
+	print("Inventory")
+	pass # Replace with function body.
