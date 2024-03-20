e80f75d1bd45fad26ee341337e62399632803e82
Nekrofage <lesanglierdesardennes@gmail.com>
2024-03-20 20:00:50 +0100

    [Add] Level 3

--- Scenes/Levels/level_03.gd
+++ Scenes/Levels/level_03.gd
@@ -2,13 +2,14 @@ extends Node2D
 
 
 @onready var player_scene = preload("res://Scenes/Player/player.tscn")
-@onready var spawnpoint_level_01 = $spawnpoint_level_01
 
 func _ready():
-	print("Come from: " + Player_data.player_previous_scene)
-
 	Player_data.player_previous_scene = self.name
 
 	var player = player_scene.instantiate()
-	player.position = spawnpoint_level_01.position
+	var node_name =  "/root/level_03/spawnpoint_level_03_" + Player_data.spawnpoint_next + "_begin"
+
+	player.position.x = get_node(node_name).position.x + Player_data.player_spawnpoint_position_x 
+	player.position.y = get_node(node_name).position.y + Player_data.player_spawnpoint_position_y - 8
+
 	add_child(player)
