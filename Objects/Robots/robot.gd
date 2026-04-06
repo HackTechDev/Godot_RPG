071c5e1f480f686ca394cb182961b988e6437f0e
Nekrofage <lesanglierdesardennes@gmail.com>
2026-04-06 11:26:41 +0200

    Revert "feat(robot): animer le robot enemy avec son spritesheet"
    This reverts commit c89ff3aa87dc432838393e9b6718a1bfc1ba1164.

--- Objects/Robots/robot.gd
+++ Objects/Robots/robot.gd
@@ -1,10 +1,6 @@
 extends RigidBody2D
 
 @onready var interaction_label = $InteractionLabel
-@onready var anim_tree: AnimationTree = $AnimationTree
-
-func _ready():
-	anim_tree.active = true
 
 func _on_interaction_area_body_entered(body):
 	if body.is_in_group("player"):
@@ -22,7 +18,3 @@ func collect():
 	Player_data.inventory.append({"type": "robot", "label": "Robot"})
 	EventBus.item_collected.emit("Robot collecté !")
 	queue_free()
-
-func set_direction(dir: Vector2):
-	anim_tree.set("parameters/Idle/blend_position", dir)
-	anim_tree.set("parameters/Move/blend_position", dir)
