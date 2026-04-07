65f4dcbe4e8d3d2970d0e5ada557877293278d23
Nekrofage <lesanglierdesardennes@gmail.com>
2026-04-07 21:35:28 +0200

    feat(hud): afficher la position du joueur dans le panneau

--- UI/hud.gd
+++ UI/hud.gd
@@ -8,7 +8,8 @@ signal home_requested
 @onready var label_health = $Panel/Margin/VBox/LabelHealth
 @onready var label_computers = $Panel/Margin/VBox/LabelComputers
 @onready var label_robots = $Panel/Margin/VBox/LabelRobots
-@onready var label_zone = $Panel/Margin/VBox/LabelZone
+@onready var label_zone     = $Panel/Margin/VBox/LabelZone
+@onready var label_position = $Panel/Margin/VBox/LabelPosition
 
 func _process(_delta):
 	label_nickname.text = "Pseudo: " + Player_data.player_nickname
@@ -16,6 +17,7 @@ func _process(_delta):
 	label_computers.text = "Ordinateurs: " + str(Player_data.computer)
 	label_robots.text = "Robots: " + str(Player_data.robot)
 	label_zone.text = "Zone: " + Player_data.player_previous_scene
+	label_position.text = "Pos: %d, %d" % [Player_data.player_pos_x, Player_data.player_pos_y]
 
 func _on_btn_sheet_pressed():
 	sheet_requested.emit()
